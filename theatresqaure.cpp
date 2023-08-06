4fd017d828030830a286a1d665fdae94393fc4e6
anuragmaityy <maityanurag2003@gmail.com>
2023-08-06 22:34:40 +0530

    Initial Commit

--- theatresqaure.cpp
+++ theatresqaure.cpp
@@ -1,9 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    unsigned long long n,m,a;
-    cin>>n>>m>>a;
-    cout<<ceil((ceil((double) n / a)) * (ceil((double)m / a)));
-    return 0;
+int main() {
+unsigned long long n,m,a;
+cin>>n>>m>>a;
+unsigned long long value=ceil((double)m/a)*ceil((double)n/a);
+cout<<value ;
+	return 0;
 }
